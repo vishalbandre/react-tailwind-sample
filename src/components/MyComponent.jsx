0adfac5290634d46dac933b511823d2e997be30b
Vishal Bandre <vshlbndr@gmail.com>
2024-08-06 15:11:36 +0530

    Initialized

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,14 @@
+// src/components/MyComponent.jsx
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <div className="bg-blue-500 text-white p-4">
+            This is a Tailwind CSS styled component.
+            <br />
+            Sample by: <a href="https://factober.com">Factober</a>
+        </div>
+    );
+};
+
+export default MyComponent;
